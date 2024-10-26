40decfbdbba5c872aeed25b81fd096771f11a985
jack-app <lico.tomato69@gmail.com>
2024-10-27 00:50:02 +0900

    検索フォーム作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,53 @@
-import styles from "./page.module.scss";
-import { OrigamiList } from "@/components/OrigamiList";
+'use client';
+import { Header } from '@/components/header';
+import styles from './page.module.scss';
+import { OrigamiList } from '@/components/OrigamiList';
+import { useState } from 'react';
+import { SearchBoxPresenter } from '@/components/search-box/presenter';
+import { ListItemProps } from '@/components/OrigamiListItem';
+import origamiData from '../models/origamiList.json';
 
 export default function Home() {
+  const items = origamiData;
+  const [filteredOrigamiList, setfilteredOrigamiList] = useState<
+    ListItemProps[] | null
+  >(null);
+  const origamiList = items.map(({ searchKeyword, ...rest }) => rest);
+
+  const handleSearch = (searchKeyword: string) => {
+    console.log('searchKeyword:', searchKeyword); // 検索キーワードの確認
+
+    // 検索キーワードが空の場合、全データを表示
+    if (searchKeyword === '') {
+      setfilteredOrigamiList(null);
+      return;
+    }
+
+    // 検索キーワードでフィルタリング
+    const newfilteredOrigamiList = items.filter((item) =>
+      item.searchKeyword.some((keyword: string) =>
+        keyword.includes(searchKeyword)
+      )
+    );
+
+    // searchKeywordを除いた結果を設定
+    const newItems = newfilteredOrigamiList.map(
+      ({ searchKeyword, ...rest }) => rest
+    );
+    setfilteredOrigamiList(newItems); // フィルタリング結果を更新
+  };
   return (
-    <main className={styles.main}>
-      <OrigamiList />
-    </main>
+    <>
+      <Header>
+        <SearchBoxPresenter handleSearch={handleSearch} />
+      </Header>
+      <main className={styles.main}>
+        {filteredOrigamiList && filteredOrigamiList.length > 0 ? (
+          <OrigamiList origamiList={filteredOrigamiList} />
+        ) : (
+          <OrigamiList origamiList={origamiList} />
+        )}
+      </main>
+    </>
   );
 }
--- src/components/search-box/presenter.tsx
+++ src/components/search-box/presenter.tsx
@@ -1,22 +1,46 @@
-"use client"
+'use client';
 
-import { TextField } from "@radix-ui/themes";
-import { HiMagnifyingGlass } from "react-icons/hi2";
-import style from "./presenter.module.scss"
+import { TextField } from '@radix-ui/themes';
+import { HiMagnifyingGlass } from 'react-icons/hi2';
+import style from './presenter.module.scss';
+import { useEffect, useState } from 'react';
 
-export const SearchBoxPresenter: React.FC = () => {
-  return(
+export type SearchBoxPresenterProps = {
+  handleSearch: (keyword: string) => void;
+};
+
+export const SearchBoxPresenter: React.FC<SearchBoxPresenterProps> = ({
+  handleSearch,
+}) => {
+  const hasVisited =
+    typeof window !== 'undefined' && !!localStorage.getItem('hasVisited');
+
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  useEffect(() => {
+    if (!hasVisited) localStorage.setItem('hasVisited', 'true');
+    else setSearchKeyword('');
+  }, []);
+
+  const handleClick = () => {
+    if (searchKeyword === '') return;
+    handleSearch(searchKeyword);
+  };
+  return (
     <>
       <div className={style.search_box}>
-        <TextField.Root placeholder="折り紙名を入力してください">
+        <TextField.Root
+          placeholder="折り紙名を入力してください"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}>
           <TextField.Slot>
             <HiMagnifyingGlass height="26" width="26" />
           </TextField.Slot>
         </TextField.Root>
       </div>
-      <div className={style.search_box_sp} onClick={() => {console.log("clicked")}}>
-        <HiMagnifyingGlass size={26}/>
-      </div>
+      <button className={style.search_box_sp} onClick={() => handleClick()}>
+        <HiMagnifyingGlass size={26} />
+      </button>
     </>
-  )
-}
+  );
+};
