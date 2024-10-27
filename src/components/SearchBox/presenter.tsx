33537f699043ffb279c0fd529f889ec2d65d865a
YutaKawamura <inheritans1026@gmail.com>
2024-10-27 10:15:32 +0900

    feat: onKeyDownの実装 && スマホ用検索アイコンの設定

--- src/components/SearchBox/presenter.tsx
+++ src/components/SearchBox/presenter.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { TextField } from '@radix-ui/themes';
-import { HiMagnifyingGlass } from 'react-icons/hi2';
-import style from './presenter.module.scss';
-import { useEffect, useState } from 'react';
+import { TextField } from "@radix-ui/themes";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import style from "./presenter.module.scss";
+import { useEffect, useState } from "react";
 
 export type SearchBoxPresenterProps = {
   handleSearch: (keyword: string) => void;
@@ -13,18 +13,28 @@ export const SearchBoxPresenter: React.FC<SearchBoxPresenterProps> = ({
   handleSearch,
 }) => {
   const hasVisited =
-    typeof window !== 'undefined' && !!localStorage.getItem('hasVisited');
+    typeof window !== "undefined" && !!localStorage.getItem("hasVisited");
 
-  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   useEffect(() => {
-    if (!hasVisited) localStorage.setItem('hasVisited', 'true');
-    else setSearchKeyword('');
+    if (!hasVisited) localStorage.setItem("hasVisited", "true");
+    else setSearchKeyword("");
   }, []);
 
   const handleClick = () => {
-    if (searchKeyword === '') return;
+    if (searchKeyword === "") return;
     handleSearch(searchKeyword);
+  };
+
+  const onKeyDown = (key: string) => {
+    switch (key) {
+      case "Enter":
+        handleSearch(searchKeyword);
+        break;
+      default:
+        break;
+    }
   };
   return (
     <>
@@ -32,15 +42,17 @@ export const SearchBoxPresenter: React.FC<SearchBoxPresenterProps> = ({
         <TextField.Root
           placeholder="おりがみのなまえを入力してください 例：つる"
           value={searchKeyword}
-          onChange={(e) => setSearchKeyword(e.target.value)}>
+          onChange={(e) => setSearchKeyword(e.target.value)}
+          onKeyDown={(e) => onKeyDown(e.key)}
+        >
           <TextField.Slot>
             <HiMagnifyingGlass height="26" width="26" />
           </TextField.Slot>
         </TextField.Root>
       </div>
+
       <button className={style.search_box_sp} onClick={() => handleClick()}>
-        {/* <HiMagnifyingGlass size={26} /> */}
-        けんさく
+        <HiMagnifyingGlass size={26} />
       </button>
     </>
   );
