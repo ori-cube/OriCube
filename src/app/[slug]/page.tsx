458a8738fa2636123c47b0aebc42204f21cc816d
YutaKawamura <inheritans1026@gmail.com>
2024-10-27 09:57:56 +0900

    fix build faild

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,18 +1,17 @@
-import fs from 'fs';
-import path from 'path';
-import React from 'react';
-import { Model } from '@/types/model';
-import { OrigamiDetail } from '@/components/OrigamiDetail';
-import { Header } from '@/components/Header';
-import { SearchBoxPresenter } from '@/components/SearchBox/presenter';
+import fs from "fs";
+import path from "path";
+import React from "react";
+import { Model } from "@/types/model";
+import { OrigamiDetail } from "@/components/OrigamiDetail";
+import { Header } from "@/components/Header";
 
 // src/modelsから、urlで指定されたモデルのデータを取得する
 async function getModelData(id: string): Promise<Model | null> {
-  const modelsDir = path.join(process.cwd(), 'src/models');
+  const modelsDir = path.join(process.cwd(), "src/models");
   const filePath = path.join(modelsDir, `${id}.json`);
 
   if (fs.existsSync(filePath)) {
-    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const fileContents = fs.readFileSync(filePath, "utf8");
     return JSON.parse(fileContents);
   }
   return null;
@@ -31,17 +30,17 @@ export default async function Page(props: { params: Params }) {
       <Header>
         <></>
       </Header>
-      <OrigamiDetail modelData={modelData}/>
+      <OrigamiDetail modelData={modelData} />
     </div>
   );
 }
 
 // ビルド時に動的ルートを生成
 export async function generateStaticParams() {
-  const modelsDir = path.join(process.cwd(), 'src/models');
+  const modelsDir = path.join(process.cwd(), "src/models");
   const fileNames = fs.readdirSync(modelsDir);
   const paths = fileNames.map((fileName) => ({
-    params: { slug: fileName.replace(/\.json$/, '') },
+    params: { slug: fileName.replace(/\.json$/, "") },
   }));
 
   return paths;
