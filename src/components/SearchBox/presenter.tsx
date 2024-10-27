b60c07f9709a6d0b9872b3c0a2100a047bec9ffa
YutaKawamura <inheritans1026@gmail.com>
2024-10-27 13:24:37 +0900

    add: スマホの検索欄を実装

--- src/components/SearchBox/presenter.tsx
+++ src/components/SearchBox/presenter.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { TextField, IconButton, Box } from "@radix-ui/themes";
-import { HiMagnifyingGlass, HiMiniXMark } from "react-icons/hi2";
+import { TextField, IconButton, Box, Flex } from "@radix-ui/themes";
+import {
+  HiMagnifyingGlass,
+  HiMiniXMark,
+  HiOutlineArrowLeft,
+} from "react-icons/hi2";
 import style from "./presenter.module.scss";
 import { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
 
 export type SearchBoxPresenterProps = {
   handleSearch: (keyword: string) => void;
@@ -16,6 +21,7 @@ export const SearchBoxPresenter: React.FC<SearchBoxPresenterProps> = ({
     typeof window !== "undefined" && !!localStorage.getItem("hasVisited");
 
   const [searchKeyword, setSearchKeyword] = useState("");
+  const [openSpSearch, setOpenSpSearch] = useState(false);
 
   useEffect(() => {
     if (!hasVisited) localStorage.setItem("hasVisited", "true");
@@ -23,8 +29,7 @@ export const SearchBoxPresenter: React.FC<SearchBoxPresenterProps> = ({
   }, []);
 
   const handleClick = () => {
-    if (searchKeyword === "") return;
-    handleSearch(searchKeyword);
+    setOpenSpSearch(!openSpSearch);
   };
 
   const onKeyDown = (key: string) => {
@@ -72,9 +77,61 @@ export const SearchBoxPresenter: React.FC<SearchBoxPresenterProps> = ({
         </Box>
       </div>
 
-      <button className={style.search_box_sp} onClick={() => handleClick()}>
-        <HiMagnifyingGlass size={26} />
-      </button>
+      {openSpSearch ? (
+        <></>
+      ) : (
+        <button className={style.search_box_sp} onClick={() => handleClick()}>
+          <HiMagnifyingGlass size={26} />
+        </button>
+      )}
+
+      {openSpSearch ? (
+        <div className={style.container}>
+          <Flex
+            align="center"
+            width="100%"
+            height="46px"
+            justify="between"
+            gap="4"
+            className={style.container_flex}
+          >
+            <HiOutlineArrowLeft
+              size={24}
+              onClick={() => {
+                handleClick();
+              }}
+            />
+            <Box width="90%">
+              <TextField.Root
+                placeholder="おりがみのなまえを入力してください 例：つる"
+                value={searchKeyword}
+                onChange={(e) => setSearchKeyword(e.target.value)}
+                onKeyDown={(e) => onKeyDown(e.key)}
+              >
+                <TextField.Slot>
+                  <HiMagnifyingGlass height="26" width="26" />
+                </TextField.Slot>
+                {searchKeyword.length != 0 ? (
+                  <TextField.Slot pr="3">
+                    <IconButton size="1" variant="ghost">
+                      <HiMiniXMark
+                        height="14"
+                        width="14"
+                        onClick={() => resetSearchKeyword()}
+                        color="#000"
+                      />
+                    </IconButton>
+                  </TextField.Slot>
+                ) : (
+                  <></>
+                )}
+              </TextField.Root>
+            </Box>
+          </Flex>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
