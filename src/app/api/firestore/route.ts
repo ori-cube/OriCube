b0ed9b2dc051976d422b3a2bff20ca2b2cf13e0e
YutaKawamura <inheritans1026@gmail.com>
2024-11-08 18:24:22 +0900

    fix: コード修正

--- src/app/api/firestore/route.ts
+++ src/app/api/firestore/route.ts
@@ -3,8 +3,6 @@ import { getFirestore } from "firebase-admin/firestore";
 import { initializeApp, getApps } from "firebase-admin/app";
 import serviceAccount from "../../../../firestore-admin-key.json";
 import { restore } from "firestore-export-import";
-// import fs from "fs";
-import data from "./data.json";
 
 const COLLECTION_NAME = "origami";
 
