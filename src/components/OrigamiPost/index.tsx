b849796ac9996d0578430e810a8ec973506d9c67
yutteee <yusaku_nakamura_aa@icloud.com>
2024-11-09 15:58:05 +0900

    Origamiの初期状態を表示

--- src/components/OrigamiPost/index.tsx
+++ src/components/OrigamiPost/index.tsx
@@ -1,10 +1,125 @@
-import React from "react";
-// import styles from "./index.module.scss";
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import styles from "./index.module.scss";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 export const OrigamiPost = () => {
-  return (
-    <div>
-      <h1>hogehoge</h1>
-    </div>
-  );
+  const initialBoard = [
+    [18, 18, 0],
+    [-18, 18, 0],
+    [-18, -18, 0],
+    [18, -18, 0],
+  ];
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const sceneRef = useRef<THREE.Scene | null>(null);
+  const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
+  const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
+  const controlsRef = useRef<OrbitControls | null>(null);
+  const raycasterRef = useRef<THREE.Raycaster | null>(null);
+
+  const frontMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color("red"),
+    side: THREE.FrontSide,
+    transparent: true,
+  });
+  const backMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color("#DFDFDF"),
+    side: THREE.BackSide,
+    transparent: true,
+  });
+
+  // シーンの初期化
+  useEffect(() => {
+    if (sceneRef.current) return; // シーンが既に初期化されている場合は何もしない
+
+    const canvas = canvasRef.current!;
+    const scene = new THREE.Scene();
+    sceneRef.current = scene;
+
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+      alpha: true,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    rendererRef.current = renderer;
+
+    const camera = new THREE.PerspectiveCamera(
+      40,
+      sizes.width / sizes.height,
+      10,
+      1000
+    );
+    camera.position.set(20, 70, 100); // 右斜め上からモデルを見るようにカメラ位置を設定
+    camera.lookAt(new THREE.Vector3(0, 0, 0)); // モデルの中心を見るようにカメラの向きを設定
+    scene.add(camera);
+    cameraRef.current = camera;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controlsRef.current = controls;
+
+    const render = () => {
+      controls.update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(render);
+    };
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(new Float32Array(initialBoard.flat()), 3)
+    );
+
+    if (initialBoard.length >= 4) {
+      const indices = [];
+      for (let j = 0; j < initialBoard.length - 2; j++) {
+        indices.push(0, j + 1, j + 2);
+      }
+      console.log("Indices:", indices);
+      geometry.setIndex(indices);
+    }
+
+    const frontMesh = new THREE.Mesh(geometry, frontMaterial);
+    const backMesh = new THREE.Mesh(geometry, backMaterial);
+    scene.add(frontMesh);
+    scene.add(backMesh);
+
+    const edgesGeometry = new THREE.EdgesGeometry(geometry);
+    const wireframeMaterial = new THREE.LineBasicMaterial({
+      color: 0x000000,
+      linewidth: 1,
+    });
+    const wireframe = new THREE.LineSegments(edgesGeometry, wireframeMaterial);
+    scene.add(wireframe);
+
+    raycasterRef.current = new THREE.Raycaster();
+
+    render();
+
+    window.addEventListener("resize", () => {
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(window.devicePixelRatio);
+    });
+
+    // canvas.addEventListener("click", (event) => {});
+
+    return () => {
+      window.removeEventListener("resize", () => {});
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} id="canvas" className={styles.model} />;
 };
