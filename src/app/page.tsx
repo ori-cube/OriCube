1f2a893a17d59aa406c1701f10ba520a1f6a79bc
YutaKawamura <inheritans1026@gmail.com>
2024-11-10 13:44:48 +0900

    add: pageにテスト用ボタンの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import styles from "./page.module.scss";
 import { OrigamiList } from "@/components/OrigamiList";
 import { Header } from "@/components/Header";
 import { OrigamiListPageProvider } from "./_provider";
+import { UploadData } from "@/components/UploadData";
+import { GetData } from "@/components/GetData";
 
 export default function Home() {
   return (
     <OrigamiListPageProvider>
       <Header enableSearch={true} />
       <main className={styles.main}>
+        <UploadData />
+        <GetData />
         <OrigamiList />
       </main>
     </OrigamiListPageProvider>
