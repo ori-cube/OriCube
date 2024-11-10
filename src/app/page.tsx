b8e3f2d7d52b98bb18249b8528294817d8990076
YutaKawamura <inheritans1026@gmail.com>
2024-11-10 19:19:42 +0900

    本来使用しないコンポーネントを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import styles from "./page.module.scss";
 import { OrigamiList } from "@/components/OrigamiList";
 import { Header } from "@/components/Header";
 import { OrigamiListPageProvider } from "./_provider";
-import { UploadData } from "@/components/UploadData";
-import { GetData } from "@/components/GetData";
 import axios from "axios";
 import { Model } from "@/types/model";
 
@@ -12,10 +10,8 @@ export default async function Home() {
   const origamiData: Model[] = response.data;
   return (
     <OrigamiListPageProvider origamiData={origamiData}>
-      <Header enableSearch={true} />
+      <Header enableSearch={true} origamiData={origamiData} />
       <main className={styles.main}>
-        <UploadData />
-        <GetData />
         <OrigamiList />
       </main>
     </OrigamiListPageProvider>
