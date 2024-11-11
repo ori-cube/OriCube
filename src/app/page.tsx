8c5327bda27ea6dfea1093492577d1f345bd725f
YutaKawamura <inheritans1026@gmail.com>
2024-11-11 18:43:09 +0900

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,19 @@ import { Model } from "@/types/model";
 export default async function Home() {
   const baseUrl = process.env.NEXT_PUBLIC_URL;
   if (!baseUrl) return <div>URL is not found</div>;
-
-  const response = await axios.get(`${baseUrl}/api/data`);
-  const origamiData: Model[] = response.data;
-  return (
-    <OrigamiListPageProvider origamiData={origamiData}>
-      <Header enableSearch={true} origamiData={origamiData} />
-      <main className={styles.main}>
-        <OrigamiList />
-      </main>
-    </OrigamiListPageProvider>
-  );
+  try {
+    const response = await axios.get(`${baseUrl}/api/data`);
+    const origamiData: Model[] = response.data;
+    return (
+      <OrigamiListPageProvider origamiData={origamiData}>
+        <Header enableSearch={true} origamiData={origamiData} />
+        <main className={styles.main}>
+          <OrigamiList />
+        </main>
+      </OrigamiListPageProvider>
+    );
+  } catch (error) {
+    console.log(error);
+    return <div>Error is occur</div>;
+  }
 }
