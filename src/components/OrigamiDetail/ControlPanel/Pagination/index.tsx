4e60ddbfc85cb6abfc0d2d4b5dd74bfb8a628f64
YutaKawamura <inheritans1026@gmail.com>
2024-11-13 16:55:22 +0900

    feat: paginationの修正

--- src/components/OrigamiDetail/ControlPanel/Pagination/index.tsx
+++ src/components/OrigamiDetail/ControlPanel/Pagination/index.tsx
@@ -65,48 +65,53 @@ export const Pagination = ({
       </button>
     );
   };
+
+  const PaginationDots_2 = () => {
+    return <HiDotsHorizontal size={8} className={style.icon_2} />;
+  };
+
+  const PaginationDots_3 = () => {
+    return <HiDotsHorizontal size={8} className={style.icon_3} />;
+  };
+
   const numList = [];
   if (count <= limit) {
     for (let i = 1; i <= count; i++) {
       numList.push(i);
     }
-  } else if (
-    currentPage == 1 ||
-    currentPage == 2
-    // currentPage == 3 ||
-    // currentPage == 4
-  ) {
+  } else if (currentPage == 1 || currentPage == 2) {
     numList.push(1);
     numList.push(2);
     numList.push(3);
-    // numList.push(4);
-    // numList.push(5);
-    numList.push("...");
-    numList.push("...");
     numList.push("...");
     numList.push(count);
-  } else if (
-    // currentPage == count - 3 ||
-    // currentPage == count - 2 ||
-    currentPage == count - 1 ||
-    currentPage == count
-  ) {
+  } else if (currentPage == 3) {
+    numList.push(1);
+    numList.push(2);
+    numList.push(3);
+    numList.push(4);
+    numList.push("..");
+    numList.push(count);
+  } else if (currentPage == count - 2) {
+    numList.push(1);
+    numList.push("..");
+    numList.push(count - 3);
+    numList.push(count - 2);
+    numList.push(count - 1);
+    numList.push(count);
+  } else if (currentPage == count - 1 || currentPage == count) {
     numList.push(1);
     numList.push("...");
-    numList.push("...");
-    numList.push("...");
-    // numList.push(count - 4);
-    // numList.push(count - 3);
     numList.push(count - 2);
     numList.push(count - 1);
     numList.push(count);
   } else {
     numList.push(1);
-    numList.push("...");
+    numList.push(".");
     numList.push(currentPage - 1);
     numList.push(currentPage);
     numList.push(currentPage + 1);
-    numList.push("...");
+    numList.push(".");
     numList.push(count);
   }
   return (
@@ -115,8 +120,12 @@ export const Pagination = ({
       {numList.map((i, index) => {
         if (typeof i === "number") {
           return <div key={index}>{PaginationNum(i)}</div>;
-        } else {
+        } else if (i === ".") {
           return <div key={index}>{PaginationDots()}</div>;
+        } else if (i === "..") {
+          return <div key={index}>{PaginationDots_2()}</div>;
+        } else if (i === "...") {
+          return <div key={index}>{PaginationDots_3()}</div>;
         }
       })}
       {PaginationNext()}
