ea383dbca7482cf5b8ceb1c48812e948fbcbca1c
yutteee <yusaku_nakamura_aa@icloud.com>
2024-11-16 02:20:39 +0900

    折り方の説明パネル

--- src/components/OrigamiPost/FoldMethodControlPanel/FoldMethodSelectPanel/index.tsx
+++ src/components/OrigamiPost/FoldMethodControlPanel/FoldMethodSelectPanel/index.tsx
@@ -1,6 +1,8 @@
 import styles from "./index.module.scss";
 import { NextStepButton } from "../ui/NextStepButton";
 import { PrevStepButton } from "../ui/PrevStepButton";
+import { FoldButton } from "../ui/FoldButton";
+import { Slider, TextArea } from "@radix-ui/themes";
 
 type Props = {
   handlePrevStep: () => void;
@@ -14,7 +16,39 @@ export const FoldMethodSelectPanel: React.FC<Props> = ({
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>折り方を選択</h2>
-      <div className={styles.buttons}>
+      <div className={styles.foldButtons}>
+        <FoldButton
+          handleClick={() => {}}
+          currentStep={0}
+          totalSteps={0}
+          isFrontSide={true}
+        />
+        <FoldButton
+          handleClick={() => {}}
+          currentStep={1}
+          totalSteps={3}
+          isFrontSide={false}
+        />
+      </div>
+      <section className={styles.h3Section}>
+        <h3 className={styles.h3}>折る角度</h3>
+        <div className={styles.sliderWrapper}>
+          0
+          <Slider
+            className={styles.slider}
+            min={0}
+            max={180}
+            size="3"
+            defaultValue={[180]}
+          />
+          180
+        </div>
+      </section>
+      <section className={styles.h3Section}>
+        <h3 className={styles.h3}>折り方の説明</h3>
+        <TextArea placeholder="半分に折る" className={styles.textArea} />
+      </section>
+      <div className={styles.stepButtons}>
         <PrevStepButton handlePrevStep={handlePrevStep} />
         <NextStepButton handleNextStep={handleNextStep} />
       </div>
---
+++ src/components/OrigamiPost/FoldMethodControlPanel/ui/FoldButton/index.tsx
@@ -0,0 +1,47 @@
+import styles from "./index.module.scss";
+import { PiArrowArcRightBold } from "react-icons/pi";
+
+type Props = {
+  handleClick: () => void;
+  currentStep: number;
+  totalSteps: number;
+  isFrontSide: boolean;
+};
+
+export const FoldButton: React.FC<Props> = ({
+  handleClick,
+  currentStep,
+  totalSteps,
+  isFrontSide,
+}) => {
+  return (
+    <button
+      className={`${styles.button} ${totalSteps > 0 && styles.active}`}
+      onClick={handleClick}
+    >
+      {isFrontSide ? (
+        <>
+          <PiArrowArcRightBold aria-hidden size={32} />
+          手前に折る
+        </>
+      ) : (
+        <>
+          <PiArrowArcRightBold
+            aria-hidden
+            size={32}
+            className={styles.backIcon}
+          />
+          奥に折る
+        </>
+      )}
+      <div className={styles.steps}>
+        {Array.from({ length: totalSteps }).map((_, index) => (
+          <div
+            key={index}
+            className={index < currentStep ? styles.active : styles.step}
+          />
+        ))}
+      </div>
+    </button>
+  );
+};
