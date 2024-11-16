e48b63fd243ccb1f573538e9b1e5aaf0083ca51b
YutaKawamura <inheritans1026@gmail.com>
2024-11-16 22:55:14 +0900

    fix: iconサイズの変更

--- src/components/Header/GoogleAuth/Logout/index.tsx
+++ src/components/Header/GoogleAuth/Logout/index.tsx
@@ -2,28 +2,28 @@ import React from "react";
 import { useSession, signOut } from "next-auth/react";
 import { Button } from "@radix-ui/themes";
 import Image from "next/image";
+import styles from "./index.module.scss";
 
 export const Logout = () => {
   const { status } = useSession();
 
   if (status === "authenticated") {
     return (
-      <div>
-        <Button
-          color="gray"
-          variant="surface"
-          highContrast
-          onClick={() => signOut()}
-        >
-          <Image
-            src="/assets/google.png"
-            alt="google icon"
-            width={20}
-            height={20}
-          />
-          ログアウト
-        </Button>
-      </div>
+      <Button
+        color="gray"
+        variant="surface"
+        highContrast
+        onClick={() => signOut()}
+        className={styles.button}
+      >
+        <Image
+          src="/assets/google.png"
+          alt="google icon"
+          width={20}
+          height={20}
+        />
+        ログアウト
+      </Button>
     );
   }
   return null;
