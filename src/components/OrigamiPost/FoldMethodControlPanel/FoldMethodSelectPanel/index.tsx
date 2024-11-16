dd23771ad829bd2a70ae28e8960f70ee2cf75aa8
yutteee <yusaku_nakamura_aa@icloud.com>
2024-11-16 23:27:15 +0900

    登録ボタンのui

--- src/components/OrigamiPost/FoldMethodControlPanel/FoldMethodSelectPanel/index.tsx
+++ src/components/OrigamiPost/FoldMethodControlPanel/FoldMethodSelectPanel/index.tsx
@@ -2,7 +2,7 @@ import styles from "./index.module.scss";
 import { NextStepButton } from "../ui/NextStepButton";
 import { PrevStepButton } from "../ui/PrevStepButton";
 import { FoldButton } from "../ui/FoldButton";
-import { Slider, TextArea } from "@radix-ui/themes";
+import { Slider, TextArea, Button } from "@radix-ui/themes";
 import React from "react";
 
 type Props = {
@@ -32,50 +32,58 @@ export const FoldMethodSelectPanel: React.FC<Props> = ({
 }) => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>折り方を選択</h2>
-      <div className={styles.foldButtons}>
-        <FoldButton
-          handleClick={handleFoldFrontSide}
-          currentStep={isFoldFrontSide ? currentNumber : 0}
-          totalSteps={totalNumber}
-          isFrontSide={true}
-          isFoldFrontSide={isFoldFrontSide}
-        />
-        <FoldButton
-          handleClick={handleFoldBackSide}
-          currentStep={isFoldFrontSide ? 0 : currentNumber}
-          totalSteps={totalNumber}
-          isFrontSide={false}
-          isFoldFrontSide={isFoldFrontSide}
-        />
-      </div>
-      <section className={styles.h3Section}>
-        <h3 className={styles.h3}>折る角度</h3>
-        <div className={styles.sliderWrapper}>
-          0
-          <Slider
-            className={styles.slider}
-            min={0}
-            max={180}
-            size="3"
-            onValueChange={(value) => {
-              setFoldAngle(value[0]);
-            }}
-            defaultValue={[180]}
-            value={[foldAngle]}
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>折り方を選択</h2>
+        <div className={styles.foldButtons}>
+          <FoldButton
+            handleClick={handleFoldFrontSide}
+            currentStep={isFoldFrontSide ? currentNumber : 0}
+            totalSteps={totalNumber}
+            isFrontSide={true}
+            isFoldFrontSide={isFoldFrontSide}
           />
-          180
+          <FoldButton
+            handleClick={handleFoldBackSide}
+            currentStep={isFoldFrontSide ? 0 : currentNumber}
+            totalSteps={totalNumber}
+            isFrontSide={false}
+            isFoldFrontSide={isFoldFrontSide}
+          />
         </div>
-      </section>
-      <section className={styles.h3Section}>
-        <h3 className={styles.h3}>折り方の説明</h3>
-        <TextArea placeholder="半分に折る" className={styles.textArea} />
-      </section>
-      <div className={styles.stepButtons}>
-        <PrevStepButton handlePrevStep={handlePrevStep} />
-        <NextStepButton handleNextStep={handleNextStep} />
+        <section className={styles.h3Section}>
+          <h3 className={styles.h3}>折る角度</h3>
+          <div className={styles.sliderWrapper}>
+            0
+            <Slider
+              className={styles.slider}
+              min={0}
+              max={180}
+              size="3"
+              onValueChange={(value) => {
+                setFoldAngle(value[0]);
+              }}
+              defaultValue={[180]}
+              value={[foldAngle]}
+            />
+            180
+          </div>
+        </section>
+        <section className={styles.h3Section}>
+          <h3 className={styles.h3}>折り方の説明</h3>
+          <TextArea placeholder="半分に折る" className={styles.textArea} />
+        </section>
+        <div className={styles.stepButtons}>
+          <PrevStepButton handlePrevStep={handlePrevStep} />
+          <NextStepButton handleNextStep={handleNextStep} />
+        </div>
       </div>
-      <button onClick={handleRegisterOrigami}>折り紙を登録</button>
+      <Button
+        onClick={handleRegisterOrigami}
+        className={styles.registerButton}
+        size="3"
+      >
+        折り紙を登録
+      </Button>
     </div>
   );
 };
