10e195eb618d0b1cc0b5c7c9357182c1165f3bd0
akaishikiso <lico.tomato69@gmail.com>
2025-04-26 21:20:02 +0900

    折り方登録時にタイトルを必須にする

--- src/components/OrigamiPost/hooks/useRegisterOrigami/index.tsx
+++ src/components/OrigamiPost/hooks/useRegisterOrigami/index.tsx
@@ -44,8 +44,14 @@ export const useRegisterOrigami: UseRegisterOrigami = ({
   const description = inputStepObject[procedureIndex.toString()].description;
 
   const handleRegisterOrigami = () => {
-    if (moveBoards.length === 0) return;
-    if (rotateAxis.length === 0) return;
+    //折り面、折り線、タイトルがない場合は登録できない。
+    if (!moveBoards.length) return;
+    if (!rotateAxis.length) return;
+    if (!origamiName.length) {
+      console.error("Origami name is required.");
+      return;
+    }
+
     // xy平面上の板のうち、z座標が大きい順に、numberOfMoveBoards枚を折る
     // それ以外の板は無条件で折る
     const { newProcedure } = decideNewProcedure({
