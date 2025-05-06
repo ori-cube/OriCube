f8f55c076f1a500e728474d25328a2afd12cbbc4
yutteee <yusaku_nakamura_aa@icloud.com>
2025-05-07 07:46:10 +0900

    折り方確認画面でのtypeを修正

--- src/components/OrigamiDetail/Three/index.tsx
+++ src/components/OrigamiDetail/Three/index.tsx
@@ -3,33 +3,38 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 import styles from "./index.module.scss";
-import { Board, Procedure, isConvolutionProcedure } from "@/types/model";
+import { Board, Procedure } from "@/types/model";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { LineGeometry } from "three/examples/jsm/Addons.js";
 import { LineMaterial } from "three/examples/jsm/Addons.js";
 import { Line2 } from "three/examples/jsm/Addons.js";
 
 type Props = {
-  model: Procedure;
+  procedure: Procedure;
   color: string;
   foldAngle: number;
   procedureIndex: number;
 };
 
 export const Three: React.FC<Props> = ({
-  model,
+  procedure,
   color,
   foldAngle,
   procedureIndex,
 }) => {
-  console.log(model);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const sceneRef = useRef<THREE.Scene | null>(null);
   const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
   const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
   const controlsRef = useRef<OrbitControls | null>(null);
 
-  const procedure = model[procedureIndex.toString()];
+  // TODO: DBが修正されたらここは削除
+  // procedureのtypeが存在しない場合、typeにBaseを設定
+  if (!procedure[procedureIndex.toString()].type) {
+    procedure[procedureIndex.toString()].type = "Base";
+  }
+
+  const stepObject = procedure[procedureIndex.toString()]; // 1ステップ分
 
   // シーンの初期化
   useEffect(() => {
@@ -108,69 +113,78 @@ export const Three: React.FC<Props> = ({
     scene.clear();
 
     // そのままの板と、回転後の板を保持
-    const boards = [...procedure.fixBoards];
+    const boards = [...stepObject.fixBoards];
     const holds_line = [];
     const theta = THREE.MathUtils.degToRad(foldAngle);
 
-    // 畳み込みの場合
-    if (isConvolutionProcedure(procedure)) {
-      const nodes = procedure.nodes.concat();
-      for (let i = 0; i < procedure.moveNodesIdx.length; i++) {
+    switch (stepObject.type) {
+      case "Base":
+        if (!stepObject.rotateAxis.length) return;
+        // 通常の折り方の場合
         holds_line.push(
           new Float32Array([
-            ...procedure.rotateAxes[i][0],
-            ...procedure.rotateAxes[i][1],
+            ...stepObject.rotateAxis[0],
+            ...stepObject.rotateAxis[1],
           ])
         );
-        const moveNode = new THREE.Vector3(...nodes[procedure.moveNodesIdx[i]]);
-        const axis = new THREE.Vector3(...procedure.rotateAxes[i][0])
-          .sub(new THREE.Vector3(...procedure.rotateAxes[i][1]))
+        const rotateAxis = new THREE.Vector3(...stepObject.rotateAxis[0])
+          .sub(new THREE.Vector3(...stepObject.rotateAxis[1]))
           .normalize();
-        const subNode = new THREE.Vector3(...procedure.rotateAxes[i][0]);
-        const rotateNode = moveNode.clone().sub(subNode);
-        rotateNode.applyAxisAngle(axis, theta);
-        rotateNode.add(subNode);
-        console.log(rotateNode);
-        nodes[procedure.moveNodesIdx[i]] = [
-          rotateNode.x,
-          rotateNode.y,
-          rotateNode.z,
-        ];
-      }
-      for (let i = 0; i < procedure.boards.length; i++) {
-        const board = procedure.boards[i];
-        const newBoard = [];
-        for (let j = 0; j < board.length; j++) {
-          const node = new THREE.Vector3(...nodes[board[j]]);
-          newBoard.push([node.x, node.y, node.z]);
-        }
-        boards.push(newBoard as Board);
-      }
-    } else {
-      if (!procedure.rotateAxis.length) return;
-      // 通常の折り方の場合
-      holds_line.push(
-        new Float32Array([
-          ...procedure.rotateAxis[0],
-          ...procedure.rotateAxis[1],
-        ])
-      );
-      const rotateAxis = new THREE.Vector3(...procedure.rotateAxis[0])
-        .sub(new THREE.Vector3(...procedure.rotateAxis[1]))
-        .normalize();
-      const subNode = new THREE.Vector3(...procedure.rotateAxis[0]);
+        const subNode = new THREE.Vector3(...stepObject.rotateAxis[0]);
 
-      for (let i = 0; i < procedure.moveBoards.length; i++) {
-        const moveBoard = procedure.moveBoards[i];
-        const newBoard = moveBoard.map((point) => {
-          const node = new THREE.Vector3(...point);
-          const rotateNode = node.clone().sub(subNode);
-          rotateNode.applyAxisAngle(rotateAxis, theta);
+        for (let i = 0; i < stepObject.moveBoards.length; i++) {
+          const moveBoard = stepObject.moveBoards[i];
+          const newBoard = moveBoard.map((point) => {
+            const node = new THREE.Vector3(...point);
+            const rotateNode = node.clone().sub(subNode);
+            rotateNode.applyAxisAngle(rotateAxis, theta);
+            rotateNode.add(subNode);
+            return [rotateNode.x, rotateNode.y, rotateNode.z];
+          });
+          boards.push(newBoard as Board);
+        }
+
+        break;
+      case "convolution":
+        const nodes = stepObject.nodes.concat();
+        for (let i = 0; i < stepObject.moveNodesIdx.length; i++) {
+          holds_line.push(
+            new Float32Array([
+              ...stepObject.rotateAxes[i][0],
+              ...stepObject.rotateAxes[i][1],
+            ])
+          );
+          const moveNode = new THREE.Vector3(
+            ...nodes[stepObject.moveNodesIdx[i]]
+          );
+          const axis = new THREE.Vector3(...stepObject.rotateAxes[i][0])
+            .sub(new THREE.Vector3(...stepObject.rotateAxes[i][1]))
+            .normalize();
+          const subNode = new THREE.Vector3(...stepObject.rotateAxes[i][0]);
+          const rotateNode = moveNode.clone().sub(subNode);
+          rotateNode.applyAxisAngle(axis, theta);
           rotateNode.add(subNode);
-          return [rotateNode.x, rotateNode.y, rotateNode.z];
-        });
-        boards.push(newBoard as Board);
-      }
+          console.log(rotateNode);
+          nodes[stepObject.moveNodesIdx[i]] = [
+            rotateNode.x,
+            rotateNode.y,
+            rotateNode.z,
+          ];
+        }
+        for (let i = 0; i < stepObject.boards.length; i++) {
+          const board = stepObject.boards[i];
+          const newBoard = [];
+          for (let j = 0; j < board.length; j++) {
+            const node = new THREE.Vector3(...nodes[board[j]]);
+            newBoard.push([node.x, node.y, node.z]);
+          }
+          boards.push(newBoard as Board);
+        }
+
+        break;
+      default:
+        const _exhaustiveCheck: never = stepObject;
+        return _exhaustiveCheck;
     }
 
     // 板を描画
@@ -215,7 +229,7 @@ export const Three: React.FC<Props> = ({
       const lineMesh = new Line2(geometry, lineMaterial);
       scene.add(lineMesh);
     });
-  }, [foldAngle, procedure]);
+  }, [foldAngle, stepObject]);
 
   return <canvas ref={canvasRef} id="canvas" className={styles.model}></canvas>;
 };
--- src/types/model.ts
+++ src/types/model.ts
@@ -2,42 +2,51 @@ export type Point = [number, number, number];
 export type Board = Point[];
 export type RotateAxis = [Point, Point] | [];
 
-export type Model = {
-  id?: string;
-  name: string;
-  imageUrl?: string;
-  searchKeyword?: string[];
-  color: string;
-  procedure: Procedure;
-};
-
-type BaseProcedure = {
+export type BaseStep = {
+  type: "Base";
+  // 折り方確認の描画に必要なパラメータ
   description: string;
   fixBoards: Board[];
   moveBoards: Board[];
   rotateAxis: RotateAxis;
+  // 折り方入力の際に必要なデータ
+  selectedPoints: Point[];
+  rightBoards: Board[];
+  leftBoards: Board[];
+  isMoveBoardsRight: boolean;
+  numberOfMoveBoards: number;
+  maxNumberOfMoveBoards: number;
+  isFoldingDirectionFront: boolean;
+  foldingAngle: number;
 };
-
-type ConvolutionProcedure = BaseProcedure & {
+// 開いて畳むやつ用
+export type ConvolutionStep = {
   type: "convolution";
+  description: string;
   nodes: number[][];
   boards: number[][];
   moveNodesIdx: number[];
   rotateAxes: number[][][];
+  fixBoards: Board[];
+};
+export type Step = BaseStep | ConvolutionStep;
+
+/*
+1: Step,
+2: Step,
+3: Step,
+...
+**/
+export type StepObject = {
+  [key: string]: Step;
 };
 
-type OtherProcedure = BaseProcedure & {
-  type?: string;
+// 最終的に保存するもの
+export type Model = {
+  id: string;
+  name: string;
+  color: string;
+  imageUrl: string;
+  searchKeyword?: string[];
+  procedure: StepObject;
 };
-
-type ProcedureEntry = ConvolutionProcedure | OtherProcedure;
-
-export type Procedure = {
-  [key: string]: ProcedureEntry;
-};
-
-export function isConvolutionProcedure(
-  procedure: ProcedureEntry
-): procedure is ConvolutionProcedure {
-  return procedure.type === "convolution";
-}
