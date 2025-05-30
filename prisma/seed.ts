807f0d393609b939150d44e493604fe6eb095331
YutaKawamura <inheritans1026@gmail.com>
2025-05-30 16:13:51 +0900

    :sparkles: feat: データのseedと，モデル定義の作成

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,36 +1,45 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma/index.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const prisma = new PrismaClient();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function main() {
   const user = await prisma.user.create({
     data: {
       name: "Ori Duel",
       email: "ori-duel@gmail.com",
-      avatarUrl: "https://example.com/avatars/ori-duel.png",
+      avatarUrl:
+        "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png",
     },
   });
 
-  const models = [
+  const baseModels = [
     {
-      name: "折り鶴",
-      color: "white",
-      imageUrl: "https://example.com/models/crane.png",
-      searchKeyword: ["origami", "crane", "fold"],
-      procedure: {
-        title: "鶴",
-      },
+      name: "トラック",
+      color: "yellow",
+      imageUrl:
+        "https://pub-2a912ed59cc94e139be4a2d207f42e8f.r2.dev/origami/images/0f1bbba5-f9c2-40ff-afb5-10b002e4cc49.png",
+      searchKeyword: ["とらっく", "トラック", "track"],
     },
     {
-      name: "船",
+      name: "よっと",
       color: "blue",
-      imageUrl: "https://example.com/models/boat.png",
-      searchKeyword: ["origami", "boat", "fold"],
-      procedure: {
-        title: "船の折り方",
-      },
+      imageUrl:
+        "https://pub-2a912ed59cc94e139be4a2d207f42e8f.r2.dev/origami/images/31f49135-dc44-4a5e-accd-7c948ecde5fa.png",
+      searchKeyword: ["よっと", "bort", "ヨット"],
     },
   ];
+
+  const models = baseModels.map((m) => {
+    const filePath = path.resolve(__dirname, "./mock", `${m.name}.json`);
+    const json = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    return { ...m, procedure: json };
+  });
 
   for (const m of models) {
     await prisma.model.create({
@@ -49,10 +58,5 @@ async function main() {
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
---
+++ src/actions/model.ts
@@ -0,0 +1,70 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { InputJsonValue } from "@prisma/client/runtime/library";
+
+interface ModelProps {
+  userId: string;
+  name: string;
+  color: string;
+  imageUrl: string;
+  searchKeyword: string[];
+  procedure: InputJsonValue;
+}
+
+export async function createModel({
+  userId,
+  name,
+  color,
+  imageUrl,
+  searchKeyword,
+  procedure,
+}: ModelProps) {
+  return await db.model.create({
+    data: {
+      userId,
+      name,
+      color,
+      imageUrl,
+      searchKeyword,
+      procedure,
+    },
+  });
+}
+
+export async function getAllModels() {
+  return await db.model.findMany();
+}
+
+export async function getModelsFromId(id: string) {
+  return await db.model.findMany({
+    where: {
+      id: id,
+    },
+  });
+}
+
+export async function getModelsFromUserId(userId: string) {
+  return await db.model.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+}
+
+export async function deleteModelFromId(id: string) {
+  return await db.model.delete({
+    where: {
+      id: id,
+    },
+  });
+}
+
+// ユーザ単位でモデルを削除したい場合に使用
+export async function deleteModelFromUserId(userId: string) {
+  return await db.model.deleteMany({
+    where: {
+      userId: userId,
+    },
+  });
+}
