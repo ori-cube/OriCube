167ea327b8260a9d08ec60d51df06e62f56e1632
Nakamura Yusaku <83484258+yutteee@users.noreply.github.com>
2025-10-22 09:00:29 +0900

    Revert "3Dモデルのテストでのmock化とドラッグ&ドロップ機能の実装"

--- src/components/v2/OrigamiPost/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * OrigamiPostV2コンポーネント用のカスタムフック群
- *
- * @description
- * - useInitScene: Three.jsシーンの初期化
- * - useDragDrop: 折り紙の頂点のドラッグ&ドロップ機能
- */
-export { useInitScene } from "./useInitScene";
-export { useDragDrop } from "./useDragDrop";
