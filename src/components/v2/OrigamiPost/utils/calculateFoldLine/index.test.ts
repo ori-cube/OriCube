1dcf478fe346260c5c82c372264355d2c16a74f5
yutteee <yusaku_nakamura_aa@icloud.com>
2025-10-27 09:14:34 +0900

    :adhesive_bandage: update: テストがx軸のみの考慮になってたので修正

--- src/components/v2/OrigamiPost/utils/calculateFoldLine/index.test.ts
+++ src/components/v2/OrigamiPost/utils/calculateFoldLine/index.test.ts
@@ -3,19 +3,19 @@ import * as THREE from "three";
 import { calculateFoldLine } from "./index";
 
 describe("calculateFoldLine", () => {
-  it("X軸方向へのドラッグから折り線情報を正しく計算できる", () => {
+  it("ドラッグから折り線情報を正しく計算できる", () => {
     const p1 = new THREE.Vector3(0, 0, 0);
-    const p2 = new THREE.Vector3(10, 0, 0);
+    const p2 = new THREE.Vector3(10, 20, 0);
     const foldLine = calculateFoldLine(p1, p2);
 
     // 中点
     expect(foldLine!.midpoint.x).toBe(5);
-    expect(foldLine!.midpoint.y).toBe(0);
+    expect(foldLine!.midpoint.y).toBe(10);
     expect(foldLine!.midpoint.z).toBe(0);
 
     // 折り線の方向ベクトル
     expect(foldLine!.direction.x).toBe(0);
-    expect(foldLine!.direction.y).toBe(-10);
+    expect(foldLine!.direction.y).toBe(10);
     expect(foldLine!.direction.z).toBe(0);
   });
 
